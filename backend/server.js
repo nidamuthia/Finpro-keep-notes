b364271d80ffebea6aab8bbdc21af2403fd40216
Nur Muhammad Thariq <thoriq.noer@gmail.com>
2022-04-20 15:18:10 +0700

    Push project

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('API is running.....')
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-const PORT = 5000
+app.use('/api/notes', require('./routes/noteRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
-app.listen(PORT, console.log(
-    `Server running in port ${PORT}`
-))
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
